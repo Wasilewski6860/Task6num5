15f9d2b27e785103995822f350f30e6a620e7960
Wasilewski6860 <trollivanovich6860@gmail.com>
2022-05-02 13:31:48 +0300

    Добавлены cvs-файлы

--- src/ru/vsu/cs/course1/tree/demo/Program.java
+++ src/ru/vsu/cs/course1/tree/demo/Program.java
@@ -2,7 +2,6 @@ package ru.vsu.cs.course1.tree.demo;
 
 import java.awt.*;
 
-import static java.lang.System.in;
 import static java.lang.System.out;
 
 import java.awt.event.ActionEvent;
@@ -21,39 +20,32 @@ public class Program {
 
     public static String[][] newReading(String path) throws Exception {
 
-        // Каждая строка массива = строке файла, и содержит информацию о отельном планшете
-
-        File file = new File(path); // Создается File, после по нему создается сканнер
+        File file = new File(path);
         Scanner scn = new Scanner(file);
         String[][] array = new String[1][1];
 
-        if (file.length() == 0) { // Проверка на пустоту файла
+        if (file.length() == 0) {
             out.println("File if empty");
             return null;
 
-        } else
-        {
+        } else {
 
-            String line = scn.nextLine(); // Сканнер считывает первую строку
+            String line = scn.nextLine();
 
-            List<String> list = new ArrayList<>(); // Создается список строк(это удобно , поскольку:
-            // а) данные нужно занести в массив, длину которого мы не знаем, а список такую проблему не ставит
-            // б) получив список с нужными значениями, не будет необходимости создавать новый сканнер и проходиться по файлу вновь(если бы мы в одном прогоне посчитали бы число строк, по нему создали бы массив и еще раз прошлись по файлу)
-            list.add(line);  // Строка заносится в список
+            List<String> list = new ArrayList<>();
 
-            while (scn.hasNextLine()) {    // Первый прогон, занесение данных в список
+            list.add(line);
+
+            while (scn.hasNextLine()) {
                 line = scn.nextLine();
                 list.add(line);
             }
 
+            String[][] helpArray = new String[list.size()][list.get(0).split(",").length];
 
-            String[][] helpArray = new String[list.size()][list.get(0).split(",").length]; // Создается строковый массив по длине списка
-
-            for (int i = 0; i < list.size(); i++) {   // Из списка в массив передаются данные
+            for (int i = 0; i < list.size(); i++) {
                 String[] newHelpArray = list.get(i).split(",");
                 for (int j=0;j<newHelpArray.length;j++){
-                    // out.println(newHelpArray[j]);
-                    //  helpArray[i][j]=Double.parseDouble(newHelpArray[j]) ;
 
                     helpArray[i][j]= newHelpArray[j];
                 }
@@ -78,7 +70,7 @@ public class Program {
     }
 
 
-    public static Map<String, Map<String, String>> toMap(String [][] list){
+    public static Map<String, Map<String, String>> toJavaMap(String [][] list){
 
         Map<String, Map<String, String>> map = new HashMap<String, Map<String, String>>();
 
@@ -116,7 +108,7 @@ public class Program {
     }
 
 
-    public static List<String> arrayToListOfKeys(String[][] array){
+    public static List<String> arrayToListOfKeys(String[][] array){ //
 
         Set<String > set = new HashSet<>();
 
@@ -189,7 +181,7 @@ public class Program {
     }
 
 
-    public static String[][] SolomaKeyToArray(String key,SimpleBSTreeMap<String , SimpleBSTreeMap<String , String> > map){
+    public static String[][] SolomatinKeyToArray(String key, SimpleBSTreeMap<String , SimpleBSTreeMap<String , String> > map){
 
       //  String[][] array = new String[map.][]
         SimpleBSTreeMap<String , String>  map2 = map.get(key);
@@ -205,7 +197,7 @@ public class Program {
         return array;
     }
 
-    public static String[][] BasicKeyToArray(String key,Map<String , Map<String , String> > map){
+    public static String[][] JavaKeyToArray(String key, Map<String , Map<String , String> > map){
 
         Map<String , String>  map2 = map.get(key);
         String[][] array = new String[map2.size()][2];
@@ -229,12 +221,11 @@ public class Program {
 
         String[][] arr = newReading(".\\text.cvs");
 
-        Map<String, Map<String, String>> map = toMap(arr);
+        Map<String, Map<String, String>> map = toJavaMap(arr);
 
 
         JFrame testFrame = new JFrame();
         JTable table = new JTable();
-   //     JTableUtils.writeArrayToJTable(MainTable, finalPCTArray);
         testFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         testFrame.getContentPane().add(table, BorderLayout.CENTER);
         testFrame.setPreferredSize(new Dimension(500,200));
@@ -248,8 +239,6 @@ public class Program {
 
         final boolean[] isBasic = {true};
 
-
-
         for (int i = 0; i< arrayToListOfKeys(arr).size(); i++){
 
             JMenuItem button = new JMenuItem(""+arrayToListOfKeys(arr).get(i));
@@ -259,12 +248,12 @@ public class Program {
                 @Override
                 public void actionPerformed(ActionEvent e) {
                     if (isBasic[0]) {
-                        Map<String, Map<String, String>> map = toMap(arr);
-                        String [][] info = BasicKeyToArray(""+arrayToListOfKeys(arr).get(finalI),map);
+                        Map<String, Map<String, String>> map = toJavaMap(arr);
+                        String [][] info = JavaKeyToArray(""+arrayToListOfKeys(arr).get(finalI),map);
                         JTableUtils.writeArrayToJTable(table, info);
                     } else {
                         SimpleBSTreeMap<String , SimpleBSTreeMap<String , String> > map = toSolomatinMap(arr);
-                        String [][] info = SolomaKeyToArray(""+arrayToListOfKeys(arr).get(finalI),map);
+                        String [][] info = SolomatinKeyToArray(""+arrayToListOfKeys(arr).get(finalI),map);
                         JTableUtils.writeArrayToJTable(table, info);
                     }
 
